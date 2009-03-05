c4645bfcd08baeb3057da072d55427203f5bf905
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-03-05 09:22:29 +0000

    API for accessing Leds
    git-svn-id: http://svn2.arsip.fajran.web.id/projects/sunspot/trunk@9 6b6f8374-0c64-0410-8549-9670e6615011

--- project/LuaSpot/src/nl/uva/np/luaspot/SunSpotLib.java
+++ project/LuaSpot/src/nl/uva/np/luaspot/SunSpotLib.java
@@ -5,9 +5,13 @@
 
 package nl.uva.np.luaspot;
 
+import com.sun.spot.sensorboard.EDemoBoard;
+import com.sun.spot.sensorboard.peripheral.ITriColorLED;
+import se.krka.kahlua.stdlib.BaseLib;
 import se.krka.kahlua.vm.JavaFunction;
 import se.krka.kahlua.vm.LuaCallFrame;
 import se.krka.kahlua.vm.LuaState;
+import se.krka.kahlua.vm.LuaTable;
 
 /**
  *
@@ -15,12 +19,90 @@ import se.krka.kahlua.vm.LuaState;
  */
 public class SunSpotLib implements JavaFunction {
 
-    public int call(LuaCallFrame callFrame, int nArguments) {
-        return 0;
+    private static ITriColorLED[] leds;
+
+    private static final int LED_ON = 0;
+    private static final int LED_OFF = 1;
+    private static final int LED_RGB = 2;
+
+    private static final int NUM_FUNCTIONS = 3;
+    private static String[] names;
+    
+    static {
+        names = new String[NUM_FUNCTIONS];
+        names[LED_ON] = "led_on";
+        names[LED_OFF] = "led_off";
+        names[LED_RGB] = "led_rgb";
+
+        leds = EDemoBoard.getInstance().getLEDs();
+    }
+
+    private int index;
+    private static SunSpotLib[] functions;
+
+    public SunSpotLib(int index) {
+        this.index = index;
     }
 
     public static void register(LuaState state) {
-
+        initFunctions();
+        LuaTable sunspot = new LuaTable();
+        state.getEnvironment().rawset("sunspot", sunspot);
+        
+        for (int i=0; i<NUM_FUNCTIONS; i++) {
+            sunspot.rawset(names[i], functions[i]);
+        }
     }
 
+    private static synchronized void initFunctions() {
+        if (functions == null) {
+            functions = new SunSpotLib[NUM_FUNCTIONS];
+            for (int i=0; i<NUM_FUNCTIONS; i++) {
+                functions[i] = new SunSpotLib(i);
+            }
+        }
+    }
+
+    public String toString() {
+        return "sunspot." + names[index];
+    }
+
+    public int call(LuaCallFrame callFrame, int nArguments) {
+        switch (index) {
+            case LED_ON: return led(LED_ON, callFrame, nArguments);
+            case LED_OFF: return led(LED_OFF, callFrame, nArguments);
+            case LED_RGB: return led(LED_RGB, callFrame, nArguments);
+            default: return 0;
+        }
+    }
+
+    private static int led(int function, LuaCallFrame callFrame, int nArguments) {
+        BaseLib.luaAssert(nArguments >= 1, "Not enough arguments");
+        
+        int pos = (int)LuaState.fromDouble(callFrame.get(0));
+        System.out.println("led: type=" + function + ", pos=" + pos);
+
+        switch (function) {
+            case LED_ON: {
+                leds[pos].setOn();
+                break;
+            }
+            case LED_OFF: {
+                leds[pos].setOff();
+                break;
+            }
+            case LED_RGB: {
+                BaseLib.luaAssert(nArguments >= 4, "Not enough arguments");
+                int r = (int)LuaState.fromDouble(callFrame.get(1));
+                int g = (int)LuaState.fromDouble(callFrame.get(2));
+                int b = (int)LuaState.fromDouble(callFrame.get(3));
+
+                leds[pos].setRGB(r, g, b);
+            }
+        }
+        
+        return 0;
+    }
+
+    
 }
