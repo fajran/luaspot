c5db5b355e90760f538259ceb4a1d6d2f4da6225
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-03-05 15:46:22 +0000

    API for Temperature and Accelerometer sensors
    git-svn-id: http://svn2.arsip.fajran.web.id/projects/sunspot/trunk@14 6b6f8374-0c64-0410-8549-9670e6615011

--- project/LuaSpot/src/nl/uva/np/luaspot/SunSpotLib.java
+++ project/LuaSpot/src/nl/uva/np/luaspot/SunSpotLib.java
@@ -6,7 +6,10 @@
 package nl.uva.np.luaspot;
 
 import com.sun.spot.sensorboard.EDemoBoard;
+import com.sun.spot.sensorboard.peripheral.IAccelerometer3D;
+import com.sun.spot.sensorboard.peripheral.ITemperatureInput;
 import com.sun.spot.sensorboard.peripheral.ITriColorLED;
+import java.io.IOException;
 import se.krka.kahlua.stdlib.BaseLib;
 import se.krka.kahlua.vm.JavaFunction;
 import se.krka.kahlua.vm.LuaCallFrame;
@@ -19,13 +22,28 @@ import se.krka.kahlua.vm.LuaTable;
  */
 public class SunSpotLib implements JavaFunction {
 
-    private static ITriColorLED[] leds;
+    private static ITriColorLED[] leds = EDemoBoard.getInstance().getLEDs();
+    private static ITemperatureInput temperature = EDemoBoard.getInstance().getADCTemperature();
+    private static IAccelerometer3D accel = EDemoBoard.getInstance().getAccelerometer();
 
+    private static final int NUM_FUNCTIONS = 13;
+    
     private static final int LED_ON = 0;
     private static final int LED_OFF = 1;
     private static final int LED_RGB = 2;
 
-    private static final int NUM_FUNCTIONS = 3;
+    private static final int TEMP_RAW = 3;
+    private static final int TEMP_CELCIUS = 4;
+    private static final int TEMP_FAHRENHEIT = 5;
+
+    private static final int ACCEL_X = 6;
+    private static final int ACCEL_Y = 7;
+    private static final int ACCEL_Z = 8;
+    private static final int ACCEL_REL_X = 9;
+    private static final int ACCEL_REL_Y = 10;
+    private static final int ACCEL_REL_Z = 11;
+    private static final int ACCEL_SET_REST = 12;
+
     private static String[] names;
     
     static {
@@ -34,7 +52,18 @@ public class SunSpotLib implements JavaFunction {
         names[LED_OFF] = "led_off";
         names[LED_RGB] = "led_rgb";
 
-        leds = EDemoBoard.getInstance().getLEDs();
+        names[TEMP_RAW] = "temp_raw";
+        names[TEMP_CELCIUS] = "temp_ceclcius";
+        names[TEMP_FAHRENHEIT] = "temp_fahrenheit";
+
+        names[ACCEL_X] = "accel_x";
+        names[ACCEL_Y] = "accel_y";
+        names[ACCEL_Z] = "accel_z";
+        names[ACCEL_REL_X] = "accel_relx";
+        names[ACCEL_REL_Y] = "accel_rely";
+        names[ACCEL_REL_Z] = "accel_relz";
+        names[ACCEL_SET_REST] = "accel_rest";
+
     }
 
     private int index;
@@ -69,9 +98,25 @@ public class SunSpotLib implements JavaFunction {
 
     public int call(LuaCallFrame callFrame, int nArguments) {
         switch (index) {
-            case LED_ON: return led(LED_ON, callFrame, nArguments);
-            case LED_OFF: return led(LED_OFF, callFrame, nArguments);
-            case LED_RGB: return led(LED_RGB, callFrame, nArguments);
+            case LED_ON: 
+            case LED_OFF: 
+            case LED_RGB:
+                return led(index, callFrame, nArguments);
+
+            case TEMP_RAW:
+            case TEMP_CELCIUS:
+            case TEMP_FAHRENHEIT:
+                return temperature(index, callFrame, nArguments);
+                
+            case ACCEL_X:
+            case ACCEL_Y:
+            case ACCEL_Z:
+            case ACCEL_REL_X:
+            case ACCEL_REL_Y:
+            case ACCEL_REL_Z:
+            case ACCEL_SET_REST:
+                return accel(index, callFrame, nArguments);
+
             default: return 0;
         }
     }
@@ -104,5 +149,68 @@ public class SunSpotLib implements JavaFunction {
         return 0;
     }
 
+    private static int temperature(int function, LuaCallFrame callFrame, int nArguments) {
+
+        try {
+            switch (function) {
+                case TEMP_RAW: {
+                    callFrame.push(new Double(temperature.getValue()));
+                    return 1;
+                }
+                case TEMP_CELCIUS: {
+                    callFrame.push(new Double(temperature.getCelsius()));
+                    return 1;
+                }
+                case TEMP_FAHRENHEIT: {
+                    callFrame.push(new Double(temperature.getFahrenheit()));
+                    return 1;
+                }
+            }
+        }
+        catch (IOException e) {
+        }
+        
+        return 0;
+    }
+
+     private static int accel(int function, LuaCallFrame callFrame, int nArguments) {
+
+        try {
+            switch (function) {
+                case ACCEL_X: {
+                    callFrame.push(new Double(accel.getTiltX()));
+                    return 1;
+                }
+                case ACCEL_Y: {
+                    callFrame.push(new Double(accel.getTiltY()));
+                    return 1;
+                }
+                case ACCEL_Z: {
+                    callFrame.push(new Double(accel.getTiltZ()));
+                    return 1;
+                }
+                case ACCEL_REL_X: {
+                    callFrame.push(new Double(accel.getRelativeAccelX()));
+                    return 1;
+                }
+                case ACCEL_REL_Y: {
+                    callFrame.push(new Double(accel.getRelativeAccelY()));
+                    return 1;
+                }
+                case ACCEL_REL_Z: {
+                    callFrame.push(new Double(accel.getRelativeAccelZ()));
+                    return 1;
+                }
+                case ACCEL_SET_REST: {
+                    accel.setRestOffsets();
+                    return 0;
+                }
+            }
+        }
+        catch (IOException e) {
+        }
+
+        return 0;
+    }
     
 }
