9fce597d109d7954f0249bc3b78b7fa8caccd305
Fajran Iman Rusadi <frusadi@science.uva.nl>
2009-03-18 22:06:17 +0000

    Fix bug
    git-svn-id: http://svn2.arsip.fajran.web.id/projects/sunspot/trunk@34 6b6f8374-0c64-0410-8549-9670e6615011

--- project/LuaSpot/lua/hotpath.lua
+++ project/LuaSpot/lua/hotpath.lua
@@ -21,9 +21,21 @@ hotpath = {
 		luaspot.unlock("hotpath")
 	end,
 
+	reset = function()
+		luaspot.lock("hotpath")
+		data = {}
+		luaspot.mem_set("hotpath", "table", data)
+		luaspot.unlock("hotpath")
+
+		sunspot.led_rgb(1, 255, 0, 0)
+		sunspot.led_on(1)
+		sunspot.sleep(1000)
+		sunspot.led_off(1)
+	end,
+
 	sort = function(a, b)
 		if a[2] ~= b[2] then
-			return a[2] < b[2]
+			return tonumber(a[2]) < tonumber(b[2])
 		end
 		return a[1] <= b[1]
 	end,
@@ -31,12 +43,13 @@ hotpath = {
 	dump = function()
 		data = luaspot.mem_get("hotpath", "table")
 		res = "c debug print"
+		addr = luaspot.get_node_addr()
 
 		if data == nil then
 			res = res .. " --"
 		else
 			for i,v in pairs(data) do
-				res = res .. " " .. v[2]
+				res = res .. " " .. v[1] .. "=" .. v[2]
 			end
 		end
 
@@ -95,11 +108,16 @@ hotpath = {
 	seqblink = function()
 		hotpath.on()
 
-		sunspot.sleep(2000)
-
+		addr = luaspot.get_node_addr()
 		nextaddr = hotpath.get_next()
+		sunspot.sleep(500)
+
+		luaspot.send_raw("broadcast", "c debug print seqblink: curr=" .. addr .. " next=" .. nextaddr)
+
+		sunspot.sleep(500)
+
 		if nextaddr ~= "" then
-			luaspot.send_raw(nextaddr, "c hotpath on")
+			luaspot.send_raw(nextaddr, "c hotpath seqblink")
 		end
 	end,
 
